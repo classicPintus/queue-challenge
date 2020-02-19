0a7f1134f691c3189214a0df6d6ac3b053041a5e
Gavino Pintus <gavino.pintus@gmail.com>
2020-02-19 13:35:10 +0100

    Comments on File Queue

--- src/main/java/com/example/file/FileQueueService.java
+++ src/main/java/com/example/file/FileQueueService.java
@@ -4,4 +4,15 @@ public class FileQueueService  {
   //
   // Task 3: Implement me if you have time.
   //
+
+    /**
+     * I don't have so much time, but basically is the same as in-memory implementation, BUT the I/O operations
+     * are often not thread safe, so we need some extra code due to the synchronization issues.
+     *
+     * Typically I use ReentrantLock for this purpose
+     *
+     * The implementation should be to save a file for each message and use a folder for each queue.
+     * I suppose to have some more constraints related to the max length of the paths and the ones related to
+     * the file names, and should be slower than in-memory implementation.
+     */
 }
