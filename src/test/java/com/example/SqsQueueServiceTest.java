42f5a7c1d32f6861c306321e6b3075350039515e
Gavino Pintus <gavino.pintus@gmail.com>
2020-02-19 13:35:55 +0100

    Now this test is useless

--- src/test/java/com/example/SqsQueueServiceTest.java
+++ src/test/java/com/example/SqsQueueServiceTest.java
@@ -1,18 +1,11 @@
 package com.example;
 
-import com.example.dto.MessageToSend;
-import com.example.dto.QueueMessage;
 import com.example.sqs.SqsQueueService;
 import com.example.test.BaseUnitTest;
 import com.example.utils.QueueNameValidator;
 import com.example.utils.VisibilityTimeoutValidator;
-import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Ignore;
-import org.junit.Test;
-
-import java.util.Optional;
-import java.util.UUID;
 
 // with right permissions this test works
 @Ignore
@@ -21,26 +14,5 @@ public class SqsQueueServiceTest extends BaseUnitTest {
     @Before
     public void setUp() {
         target = new SqsQueueService(new QueueNameValidator(), new VisibilityTimeoutValidator());
-    }
-
-    @Test
-    public void testWholeWorkflow() {
-
-        String queueName = UUID.randomUUID().toString() + ".fifo";
-        target.createQueue(queueName, 1);
-
-        try {
-
-            target.push(queueName, new MessageToSend("prova"));
-
-            Optional<QueueMessage> messageReceived = target.pull(queueName);
-            Assert.assertTrue(messageReceived.isPresent());
-            Assert.assertEquals("prova", messageReceived.get().getBody());
-            Assert.assertNotNull(messageReceived.get().getUniqueIdentifier());
-
-            target.delete(queueName, messageReceived.get());
-        } finally {
-            target.deleteQueue(queueName);
-        }
     }
 }
